9f6bc514aafece25bb75aa485c5fd56dff8952d2
Mohammed Saleh <mohsalehwork@gmail.com>
2025-06-10 18:03:14 +0400

    added controllers for getting regions and get region by id

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        //Get All Regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Get all regions from the database / domain model
+            var regionsDomain = dbContext.Regions.ToList();
+
+            //map domain model to DTOs
+            var regionsDto = new List<Models.DTO.RegionDto>();
+            foreach (var region in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl
+                });
+            }
+            //return the DTOs
+            return Ok(regionsDto);
+        }
+
+        // Get Region by Id
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //Get the region from the database / domain model
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            //map domain model to DTOs
+            var regionDto = new RegionDto()
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                RegionImageUrl = region.RegionImageUrl
+            };
+            //return the DTO
+            return Ok(regionDto);
+        }
+    }
+}
