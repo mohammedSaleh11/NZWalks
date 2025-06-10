bcc9eef4796c312a558fcb3dd6ed44c4d0959e50
Mohammed Saleh <mohsalehwork@gmail.com>
2025-06-10 13:12:35 +0400

    Initial commit without .vs files

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace NZWalks.API.Controllers
+{
+
+    //https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents() {
+            string[] studentNames = new string[] { "John Doe", "Jane Smith", "Alice Johnson" };
+
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; } // Length in kilometers
+        public string? WalkImageUrl { get; set; } // URL to an image of the walk
+        public Guid RegionId { get; set; } // Foreign key to Region
+        public Guid DifficultyId { get; set; } // Foreign key to Difficulty
+        // Navigation properties
+        public  Region Region { get; set; }
+        public  Difficulty Difficulty { get; set; }
+    }
+}
